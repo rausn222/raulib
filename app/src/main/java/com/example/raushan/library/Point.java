7a26912046a0b8e436f312fd48a790cf692a2ed6
Raushan Kumar <rausn222@gmail.com>
2018-05-26 13:23:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/raushan/library/Point.java
@@ -0,0 +1,20 @@
+package com.example.raushan.library;
+
+public class Point {
+
+        float mLatitude;
+        float mLongitude;
+
+        public Point(float latitude, float longitude) {
+            mLatitude = latitude;
+            mLongitude = longitude;
+        }
+
+        public float getLatitude() {
+            return mLatitude;
+        }
+
+        public float getLongitude() {
+            return mLongitude;
+        }
+}
